92a705713be4761715f3b00fcae6f3f59941cccf
Thiago Mendes Coscrato <thiago.mendes-e@hotmail.com>
2023-11-17 00:00:25 -0300

    Update db.js

--- BkE/src/db.js
+++ BkE/src/db.js
@@ -1,7 +1,13 @@
 const sequelize = require('sequelize');
 
 const database = new sequelize('MuOnlineS4', 'sa', '123456', {
-    dialect:'mssql',host:'localhost',porta:1433
+    dialect:'mssql',host:'localhost', porta: 1433,
+    options: {
+        enableArithAbort: false,
+        cryptoCredentialsDetails: {
+            minVersion: 'TLSv1'
+        }
+    }
 });
 
 database.sync();
